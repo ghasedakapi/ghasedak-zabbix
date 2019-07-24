a58a10f2fafdf5d803392480bacad67e1aa547d2
yaghobi <mostafa.yaghobi@gmail.com>
2019-07-24 10:43:20 +0430

    Using Bash is specified explicitly. Logging is added.

--- sms.sh
+++ sms.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Get the API KEY from your ghasedak.io account
 readonly API_KEY=''
 
@@ -5,8 +7,10 @@ to="$1" # Should contain the value of {ALERT.SENDTO}
 from="$2" # Should contain your Ghasedak phone number
 text="$3" # Should container the value of {ALERT.MESSAGE} or {ALERT.SUBJECT}
 
+echo "$(date) ${text} ${from} $to" >> /tmp/log.txt
+
 curl -X POST "http://api.ghasedak.io/v2/sms/send/simple" \
 	-H "apikey: ${API_KEY}" \
 	-H 'cache-control: no-cache' \
 	-H 'content-type: application/x-www-form-urlencoded' \
-	-d "message=${text} &receptor=${to}"
+	-d "message=${text}&linenumber=${from}&receptor=${to}" 2>&1 >> /tmp/log.txt
